1bdb882a977784bfc1245b16eb3661583611336f
Olena Stepaniuk <helen.stepaniuk@gmail.com>
2021-01-24 19:17:37 +0200

    Add rx data source

--- Formula1/Controllers/DetailsViewController.swift
+++ Formula1/Controllers/DetailsViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import RxDataSources
+import Differentiator
 
 class DetailsViewController: UIViewController {
     
@@ -17,18 +19,61 @@ class DetailsViewController: UIViewController {
         return tableView
     }()
     
+    var viewModel = PilotsViewModel()
+    
+    var round = ""
+    
+    var year = ""
+    
+    var sections = PublishSubject<[SectionModel]>()
+    
+    let disposeBag = DisposeBag()
+    
+    init(year: String, round: String) {
+        self.year = year
+        self.round = round
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
-        tableView.register(Formula1Cell.self, forCellReuseIdentifier: K.CellIdentifier.formula1Cell)
-        
-        // MARK: - Delegates
-        tableView.dataSource = self
-        tableView.delegate = self
-        
+        tableView.rx.setDelegate(self).disposed(by: disposeBag)
+        //        tableView.register(Formula1Cell.self, forCellReuseIdentifier: K.CellIdentifier.formula1Cell)
+        bindTableView()
         
         title = "Details"
         
+        print("\(year) \(round)")
+        
+        
+        
+        let dataSource = DetailsViewController.dataSource()
+        
+        sections.bind(to: tableView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+        
+        //        viewModel.fetchNewPilots(apiRouterCase: .getPilotsInSeasonInRound(year: year, round: round))
+    }
+    
+    func bindTableView() {
+        tableView.register(Formula1Cell.self, forCellReuseIdentifier: K.CellIdentifier.formula1Cell)
+        
+        
+        viewModel.pilots
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { items in
+                let tmp1 = items.compactMap({SectionModelItem.raceSectionItem(item: $0)})
+                let tmp2 = items.compactMap({SectionModelItem.pilotSectionItem(item: $0)})
+                self.sections.onNext([.raceSection(title: "", items: [tmp1.first ?? .raceSectionItem(item: PilotModel())]), .pilotSection(title: "Results", items: tmp2)])
+            }).disposed(by: disposeBag)
+        
+        viewModel.fetchData(apiRouterCase: .getPilotsInSeasonInRound(year: year, round: round))
+//        viewModel.fetchNewPilots(apiRouterCase: .getPilotsInSeasonInRound(year: year, round: round))
     }
 }
 
@@ -48,44 +93,148 @@ extension DetailsViewController {
     }
 }
 
+extension DetailsViewController {
+    static func dataSource() -> RxTableViewSectionedReloadDataSource<SectionModel> {
+        return RxTableViewSectionedReloadDataSource<SectionModel>( configureCell: { (dataSources, tableView, ip, _) -> UITableViewCell in
+                        switch dataSources[ip] {
+            
+                        case .raceSectionItem(item: let item):
+                            if ip.section == 0 {
+                                let cell = tableView.dequeueReusableCell(withIdentifier: K.CellIdentifier.formula1Cell, for: ip) as! Formula1Cell
+                                cell.topLabelText = item.raceName + "-" + item.round
+                                cell.bottomLabelText = item.raceName + "   " + item.date
+                                return cell
+                            }
+                            return UITableViewCell()
+                        case .pilotSectionItem(item: let item):
+                            if ip.section == 1 {
+                                let cell = tableView.dequeueReusableCell(withIdentifier: K.CellIdentifier.formula1Cell, for: ip) as! Formula1Cell
+                                cell.topLabelText = "\(item.givenName) \(item.familyName) \(item.permanentNumber)"
+                                cell.bottomLabelText = item.time
+                                return cell
+                            }
+                            return UITableViewCell()
+                        }
+//            if ip.section < 1 {
+//                switch dataSources[ip] {
+//
+//                case .raceSectionItem(item: let item):
+//                    let cell = tableView.dequeueReusableCell(withIdentifier: K.CellIdentifier.formula1Cell, for: ip) as! Formula1Cell
+//                    cell.topLabelText = item.raceName + "-" + item.round
+//                    cell.bottomLabelText = item.raceName + "   " + item.date
+//                    return cell
+//                default:
+//                    return UITableViewCell()
+//                }
+//            } else {
+//                switch dataSources[ip] {
+//                case .pilotSectionItem(item: let item):
+//                        let cell = tableView.dequeueReusableCell(withIdentifier: K.CellIdentifier.formula1Cell, for: ip) as! Formula1Cell
+//                        cell.topLabelText = "\(item.givenName) \(item.familyName) \(item.permanentNumber)"
+//                        cell.bottomLabelText = item.time
+//                        return cell
+//                default:
+//                    return UITableViewCell()
+//                }
+//            }
+            
+            
+        },  titleForHeaderInSection: { dataSource, index in
+            let section = dataSource[index]
+            return section.title
+        }
+        )    }
+}
+
+enum SectionModel {
+    case raceSection(title: String, items: [SectionModelItem])
+    case pilotSection(title: String, items: [SectionModelItem])
+}
+
+enum SectionModelItem {
+    case raceSectionItem(item: PilotModel)
+    case pilotSectionItem(item: PilotModel)
+}
+
+
+extension SectionModel: SectionModelType {
+    typealias Item = SectionModelItem
+    
+    
+    init(original: SectionModel, items: [Item]) {
+        switch original {
+        
+        case .raceSection(title: let title, items: let items):
+            self = .raceSection(title: title, items: items)
+        case .pilotSection(title: let title, items: let items):
+            self = .pilotSection(title: title, items: items)
+        }
+    }
+    
+    var items: [SectionModelItem] {
+        get {
+            switch self {
+            
+            case .raceSection(title: _, items: let items):
+                return items
+            case .pilotSection(title: _, items: let items):
+                return items.map({$0})
+            }
+        }
+    }
+}
+
+extension SectionModel {
+    var title: String {
+        switch self {
+        
+        case .raceSection(title: let title, items: _):
+            return title
+        case .pilotSection(title: let title, items: _):
+            return title
+        }
+    }
+}
+
+
 // MARK: - Table View Data Source and Delegate methods
-extension DetailsViewController: UITableViewDelegate, UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return 1
-        }
-        
-        return 10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 0 {
-            let infoCell = tableView.dequeueReusableCell(withIdentifier: K.CellIdentifier.formula1Cell, for: indexPath) as! Formula1Cell
-            infoCell.topLabelText = "1898-5475-3  jfhdjkfh"
-            infoCell.bottomLabelText = "hjdkshf kjdhfkdfj"
-            infoCell.accessoryType = .disclosureIndicator
-            return infoCell
-        }
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.CellIdentifier.formula1Cell, for: indexPath) as! Formula1Cell
-        cell.topLabelText = "4563874568374568347 48574"
-        cell.bottomLabelText = "sdfsdfsdf kjdhfkdfj"
-        cell.accessoryType = .disclosureIndicator
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if section == 1 {
-            return "Results"
-        }
-        
-        return nil
-    }
+extension DetailsViewController: UITableViewDelegate {
+    //    func numberOfSections(in tableView: UITableView) -> Int {
+    //        return 2
+    //    }
+    //
+    //    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //        if section == 0 {
+    //            return 1
+    //        }
+    //
+    //        return 10
+    //    }
+    //
+    //    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //        if indexPath.section == 0 {
+    //            let infoCell = tableView.dequeueReusableCell(withIdentifier: K.CellIdentifier.formula1Cell, for: indexPath) as! Formula1Cell
+    //            infoCell.topLabelText = "1898-5475-3  jfhdjkfh"
+    //            infoCell.bottomLabelText = "hjdkshf kjdhfkdfj"
+    //            infoCell.accessoryType = .disclosureIndicator
+    //            return infoCell
+    //        }
+    //
+    //        let cell = tableView.dequeueReusableCell(withIdentifier: K.CellIdentifier.formula1Cell, for: indexPath) as! Formula1Cell
+    //        cell.topLabelText = "4563874568374568347 48574"
+    //        cell.bottomLabelText = "sdfsdfsdf kjdhfkdfj"
+    //        cell.accessoryType = .disclosureIndicator
+    //
+    //        return cell
+    //    }
+    //
+    //    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    //        if section == 1 {
+    //            return "Results"
+    //        }
+    //
+    //        return nil
+    //    }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 50
