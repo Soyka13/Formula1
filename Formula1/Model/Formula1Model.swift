984032d7774f728b84f97398eba785f2b8602465
Olena Stepaniuk <helen.stepaniuk@gmail.com>
2021-01-24 19:14:52 +0200

    Update data models

---
+++ Formula1/Model/Formula1Model.swift
@@ -0,0 +1,52 @@
+//
+//  Formula1Model.swift
+//  Formula1
+//
+//  Created by Olena Stepaniuk on 24.01.2021.
+//
+
+import Foundation
+
+struct Pilot: Decodable {
+    var permanentNumber: String
+    var givenName: String
+    var familyName: String
+    var url: String
+}
+
+struct PilotTime: Decodable {
+    let time: String
+    let millis: String
+}
+
+struct RaceResult: Decodable {
+    let Driver: Pilot
+    let Time: PilotTime?
+}
+
+struct Race: Decodable {
+    let raceName: String
+    let season: String
+    let round: String
+    let url: String
+    let date: String
+    let Results: [RaceResult]
+    
+    func getPilotsDataModel() -> [PilotModel] {
+        return Results.compactMap({
+            PilotModel(driver: $0.Driver, raceName: raceName, season: season, round: round, raceUrl: url, date: date, time: $0.Time?.time ?? "")
+        })
+    }
+}
+
+struct RaceTable: Decodable {
+    let Races: [Race]
+}
+
+struct RData: Decodable {
+    let RaceTable: RaceTable
+}
+
+struct MRData: Decodable {
+    let MRData: RData
+}
