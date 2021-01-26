8e978d39ab946480efad63746775ae570ff2026c
Olena Stepaniuk <helen.stepaniuk@gmail.com>
2021-01-26 18:50:09 +0200

    Add parameters for request

--- Formula1/Networking/ApiRouter.swift
+++ Formula1/Networking/ApiRouter.swift
@@ -9,11 +9,12 @@ import Foundation
 import Alamofire
 import RxSwift
 
-struct Formila1ApiConstants {
+struct Formula1ApiConstants {
     static let baseURL = "https://ergast.com/"
     
     struct Parameters {
-        
+        static let limit = "limit"
+        static let offset = "offset"
     }
     
     enum HttpHeaderField: String {
@@ -28,23 +29,23 @@ struct Formila1ApiConstants {
 
 enum ApiRouter: URLRequestConvertible {
     
-    case getPilotsWinnersInSeason(year: String)
+    case getPilotsWinnersInSeason(year: String, limit: Int = 50, offset: Int = 0)
     
-    case getPilotsWinnersInSeasonInRound(year: String, round: String)
+    case getPilotsWinnersInSeasonInRound(year: String, round: String, limit: Int = 50, offset: Int = 0)
     
-    case getPilotsInSeasonInRound(year: String, round: String)
+    case getPilotsInSeasonInRound(year: String, round: String, limit: Int = 50, offset: Int = 0)
     
-    case getPilotsInSeason(year: String)
+    case getPilotsInSeason(year: String, limit: Int = 50, offset: Int = 0)
     
     private var path: String {
         switch self {
-        case .getPilotsWinnersInSeason(year: let year):
+        case .getPilotsWinnersInSeason(year: let year, limit: _, offset: _):
             return "api/f1/\(year)/results/1.json"
-        case .getPilotsWinnersInSeasonInRound(year: let year, round: let round):
+        case .getPilotsWinnersInSeasonInRound(year: let year, round: let round, limit: _, offset: _):
             return "api/f1/\(year)/\(round)/results/1.json"
-        case .getPilotsInSeason(year: let year):
+        case .getPilotsInSeason(year: let year, limit: _, offset: _):
             return "api/f1/\(year)/results.json"
-        case .getPilotsInSeasonInRound(year: let year, round: let round):
+        case .getPilotsInSeasonInRound(year: let year, round: let round, limit: _, offset: _):
             return "api/f1/\(year)/\(round)/results.json"
         }
         
@@ -58,13 +59,27 @@ enum ApiRouter: URLRequestConvertible {
         }
     }
     
+    private var parameters: Parameters? {
+        switch self {
+        
+        case .getPilotsWinnersInSeason(year: _, limit: let limit, offset: let offset):
+            return [Formula1ApiConstants.Parameters.limit : limit, Formula1ApiConstants.Parameters.offset: offset]
+        case .getPilotsWinnersInSeasonInRound(year: _, round: _, limit: let limit, offset: let offset):
+            return [Formula1ApiConstants.Parameters.limit : limit, Formula1ApiConstants.Parameters.offset: offset]
+        case .getPilotsInSeasonInRound(year: _, round: _, limit: let limit, offset: let offset):
+            return [Formula1ApiConstants.Parameters.limit : limit, Formula1ApiConstants.Parameters.offset: offset]
+        case .getPilotsInSeason(year: _, limit: let limit, offset: let offset):
+            return [Formula1ApiConstants.Parameters.limit : limit, Formula1ApiConstants.Parameters.offset: offset]
+        }
+    }
+    
     func asURLRequest() throws -> URLRequest {
-        let url = try Formila1ApiConstants.baseURL.asURL()
+        let url = try Formula1ApiConstants.baseURL.asURL()
         
         var request = URLRequest(url: url.appendingPathComponent(path))
         request.httpMethod = method.rawValue
-        request.setValue(Formila1ApiConstants.ContentType.json.rawValue, forHTTPHeaderField: Formila1ApiConstants.HttpHeaderField.contentType.rawValue)
-        request.setValue(Formila1ApiConstants.ContentType.json.rawValue, forHTTPHeaderField: Formila1ApiConstants.HttpHeaderField.acceptType.rawValue)
+        request.setValue(Formula1ApiConstants.ContentType.json.rawValue, forHTTPHeaderField: Formula1ApiConstants.HttpHeaderField.contentType.rawValue)
+        request.setValue(Formula1ApiConstants.ContentType.json.rawValue, forHTTPHeaderField: Formula1ApiConstants.HttpHeaderField.acceptType.rawValue)
         
         let encoding: ParameterEncoding = {
             switch method {
@@ -75,6 +90,6 @@ enum ApiRouter: URLRequestConvertible {
             }
         }()
         
-        return try encoding.encode(request, with: nil)
+        return try encoding.encode(request, with: parameters)
     }
 }
